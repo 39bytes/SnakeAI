6fa6fc09ce4cdb00de6930f2bb7d3286efffacd9
Jeff <zhangjeff2014@gmail.com>
2021-07-17 17:57:27 -0300

    refactor values into constants

--- src/constants.py
+++ src/constants.py
@@ -1,3 +1,6 @@
-NN_SIZES = (24, 20, 4) # The neural network structure
-SCALE = 16 # Pixels per grid square
-SIZE = 256 # Size of grid in pixels
+MAX_GENERATION = 150
+MAX_FPS = 1000
+POPULATION_SIZE = 300
+SCALE = 16  # Pixels per grid square
+SIZE = 256  # Size of grid in pixels
+NN_SIZES = (24, 20, 4)  # The neural network structure
