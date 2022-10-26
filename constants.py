597a7f4b5b5d919c773a5c4d8195f7c5178ace08
Jeff Zhang <zhangjeff2014@gmail.com>
2022-10-26 09:03:20 -0400

    add second hidden layer, increase population size

--- constants.py
+++ constants.py
@@ -1,7 +1,7 @@
 MAX_GENERATION = 200
 DEFAULT_FPS = 15
 MAX_FPS = 10000
-POPULATION_SIZE = 300
+POPULATION_SIZE = 400
 SCALE = 16  # Pixels per grid square
 SIZE = 256  # Size of grid in pixels
-NN_SHAPE = (24, 20, 4)  # The neural network structure
+NN_SHAPE = (24, 14, 14, 4)  # The neural network structure
